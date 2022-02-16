3a1e53d5b8708dd864007189a30d5ed77bcdb6de
handokoadjipangestu <handokoadjipangestu@gmail.com>
2022-02-16 15:46:39 +0700

    revision - change single quotes to double quotes

--- public_html/source/js/script.js
+++ public_html/source/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     var nav = $(".navbar.fixed-top");
-    nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+    nav.toggleClass("scrolled", $(this).scrollTop() > nav.height());
   });
 });
